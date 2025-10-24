52d22905f9c4cb1a24640b4367d8b08d00611697
StupidBoyfriend <118992198+jamesmainer47@users.noreply.github.com>
2025-10-24 12:48:59 +0300

    Fix script tag formatting in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,7 +63,8 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(HoldOnPlayData) }}
         />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9891580637314161"
-     crossorigin="anonymous"></script>
+     crossorigin="anonymous"
+        />
       </Head>
     <html lang="en">
       <body
