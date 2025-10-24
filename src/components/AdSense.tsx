9d62cdbb2e80b1e7a89bf3959caae696b1672940
StupidBoyfriend <118992198+jamesmainer47@users.noreply.github.com>
2025-10-24 14:56:06 +0300

    Create AdSense component with prop for publisher ID

---
+++ src/components/AdSense.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type AdSenseTypes = {
+  pId: string;
+}
+
+const AdSense = ({ pId }: AdSenseTypes) => {
+  return (
+    <div>AdSense</div>
+  )
+}
+
+export default AdSense
