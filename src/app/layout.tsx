e81541251b67384fd29bc813770b236eec2a10d6
StupidBoyfriend <118992198+jamesmainer47@users.noreply.github.com>
2025-10-24 12:44:37 +0300

    Add Google Ads script to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,6 +62,8 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(HoldOnPlayData) }}
         />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9891580637314161"
+     crossorigin="anonymous"></script>
       </Head>
     <html lang="en">
       <body
