c693d7da1686f21f9ddc86b3a50ffe76da596795
StupidBoyfriend <118992198+jamesmainer47@users.noreply.github.com>
2025-10-24 17:52:56 +0300

    Update heading in secondSection component
    Removed pricing information from the heading.

--- src/components/ui/secondSection.tsx
+++ src/components/ui/secondSection.tsx
@@ -16,7 +16,7 @@ export default function SecondSection(){
 ;    return (
         <div ref={boxRef} className={`bg-black boxile2 flex relative flex-col justify-center items-center  p-4 text-center text-2xl ${theme === "dark" ? "bg-black text-white" : "bg-white text-black"}`}>
             <div className="flex flex-col lg:px-80 p-2">
-            <h1 className="lg:text-6xl font-bold text-4xl"><span className="text-blue-400">Unlimited Distribution</span> Starting at Tsh: 77,000/=</h1>
+            <h1 className="lg:text-6xl font-bold text-4xl"><span className="text-blue-400">Unlimited Distribution</span> </h1>
             <p className="text-justify text-sm lg:text-xl">Increase the reach of your music across the most popular stores & platforms like Spotify, Apple Music, TikTok, Youtube and many more. Empower yourself with unlimited distribution opportunities, and get your music heard by a global audience</p>
             <p className="text-justify text-sm lg:text-xl">Keep 100% ownership of your music, maintaining creative control and authority in your music career</p>
             <div className="flex self-center bg-black rounded-b-xl flex-wrap items-center justify-center p-4">{logoz.map((src) => (
