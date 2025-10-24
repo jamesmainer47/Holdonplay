26d6711c426c87c1359b57250101200d2451807e
StupidBoyfriend <118992198+jamesmainer47@users.noreply.github.com>
2025-10-24 16:45:25 +0300

    Fix script tag attribute quotes in AdSense component

--- src/components/AdSense.tsx
+++ src/components/AdSense.tsx
@@ -10,7 +10,7 @@ const AdSense = ({ pId }: AdSenseTypes) => {
       async 
       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9891580637314161"
       crossorigin="anonymous"
-      strategy='afterInteractive'
+      strategy="afterInteractive"
     />
   )
 }
