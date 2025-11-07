41358ff03a54ef2ecc5b1277e665adcacf6573fa
StupidBoyfriend <118992198+jamesmainer47@users.noreply.github.com>
2025-11-07 15:51:01 +0300

    Update AdSense integration to use Script tag
    Replaced AdSense component with Script tag for Google Ads.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import Chatbot from "@/components/ui/hopAssistant";
 import ThemeProvider from "@/components/ui/ThemeProvider";
 import { useTheme } from "@/components/ui/ThemeProvider";
 import Head from "next/head";
-import AdSense from "@/components/AdSense";
+import Script from "next/script";
 
 //export const metadata: Metadata = {
 //  title: "HoldONPlay | A Music Distribution Company",
@@ -63,7 +63,8 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(HoldOnPlayData) }}
         />
-        <AdSense pId="ca-pub-9891580637314161"/>
+        <Script async src={ 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9891580637314161' }
+     crossOrigin="anonymous"/>
       </Head>
     <html lang="en">
       <body
