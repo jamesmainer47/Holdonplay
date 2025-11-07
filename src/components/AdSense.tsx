c85b274d300e31f703e90b1fc54d8071027f0be0
StupidBoyfriend <118992198+jamesmainer47@users.noreply.github.com>
2025-11-07 12:31:54 +0300

    Update AdSense.tsx

--- src/components/AdSense.tsx
+++ src/components/AdSense.tsx
@@ -12,7 +12,7 @@ const AdSense = ({ pId }: AdSenseTypes) => {
       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9891580637314161"
       crossorigin="anonymous"
       strategy="afterInteractive"
-    /Script>
+    />
   )
 }
 
