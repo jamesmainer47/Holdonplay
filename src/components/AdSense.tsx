194be06b2e7494fed43284a2a57c79a1fe3e2e12
StupidBoyfriend <118992198+jamesmainer47@users.noreply.github.com>
2025-11-07 12:18:28 +0300

    Replace script tag with Script component

--- src/components/AdSense.tsx
+++ src/components/AdSense.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Script from 'script';
 
 type AdSenseTypes = {
   pId: string;
@@ -6,12 +7,12 @@ type AdSenseTypes = {
 
 const AdSense = ({ pId }: AdSenseTypes) => {
   return (
-    <script 
+    <Script 
       async 
       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9891580637314161"
       crossorigin="anonymous"
       strategy="afterInteractive"
-    />
+    /Script>
   )
 }
 
