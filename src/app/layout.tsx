9f71d433a821170ca3ca87109a246d1e963629cc
StupidBoyfriend <118992198+jamesmainer47@users.noreply.github.com>
2025-11-07 11:31:12 +0300

    Replace script tag with Script component from Next.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import Chatbot from "@/components/ui/hopAssistant";
 import ThemeProvider from "@/components/ui/ThemeProvider";
 import { useTheme } from "@/components/ui/ThemeProvider";
 import Head from "next/head";
+import Script from "next/script";
 import AdSense from "@/components/AdSense";
 
 //export const metadata: Metadata = {
@@ -59,7 +60,7 @@ export default function RootLayout({
   return (
     <>
       <Head>
-        <script
+        <Script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(HoldOnPlayData) }}
         />
